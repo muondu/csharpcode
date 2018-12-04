74bc0927937bb03262893151a15183b4b063cc82
muondu <munenemuondu@gmail.com>
2018-12-04 12:17:40 +0300

    Forntnight

---
+++ helloWorld/goib/goib/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace goib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fortnight Is Awsome!");
+            Console.Write("Press any key to continue");
+            Console.ReadKey(true);
+        }
+    }
+}
