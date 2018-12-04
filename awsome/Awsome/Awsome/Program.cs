e7b31c475fe3d5f83ac44a211b4ffc0917bab231
muondu <munenemuondu@gmail.com>
2018-12-04 18:53:27 +0300

    I have added all my files

---
+++ awsome/Awsome/Awsome/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace nameInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name?");
+            var Name = Console.ReadLine();
+            Console.WriteLine("\nWhat is your age?");
+            var Age = Console.ReadLine();
+            Console.WriteLine($"/n Hello,{Name}, Welcome to my game!");
+            Console.Write("Press any key to exit ");
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
---
+++ nameInput/nameInput/nameInput/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace nameInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name?");
+            var Name = Console.ReadLine();
+            Console.WriteLine($"/n Hello,{Name}, Welcome to my game!");
+            Console.Write("Press any key to exit ");
+            Console.ReadKey(true);
+
+            
+        }
+    }
+}
+using System;
+
+namespace nameInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name?");
+            var Name = Console.ReadLine();
+            Console.WriteLine($"/n Hello,{Name}, Welcome to my game!");
+            Console.Write("Press any key to exit ");
+            Console.ReadKey(true);
+
+            
+        }
+    }
+}
